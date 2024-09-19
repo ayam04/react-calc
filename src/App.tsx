fbbc909951521b774a37aab8bb8701fc21cccab8
ayam04 <ayamullahkhan04@gmail.com>
2024-09-20 01:54:48 +0530

    new

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Instructions from './components/Instructions.tsx';
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      {/* <Calculator /> */}
+      <Calculator />
       <Instructions />
     </div>
   );
